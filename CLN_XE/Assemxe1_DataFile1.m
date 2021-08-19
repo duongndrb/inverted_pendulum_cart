914d9d95a00584ed0b6cbbe31cd6e434e47bfb1f
Nguyễn Đình Dương <47983821+duongndrb@users.noreply.github.com>
2021-08-19 09:35:48 +0700

    Add files via upload

---
+++ CLN_XE/Assemxe1_DataFile1.m
@@ -0,0 +1,133 @@
+% Simscape(TM) Multibody(TM) version: 6.1
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(7).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(7).angle = 0.0;
+smiData.RigidTransform(7).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(7).ID = '';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [-149.99999999999991 0 0];  % mm
+smiData.RigidTransform(1).angle = 0;  % rad
+smiData.RigidTransform(1).axis = [0 0 0];
+smiData.RigidTransform(1).ID = 'UCS[xeCart*:*Default:cord_force]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [0 500 0];  % mm
+smiData.RigidTransform(2).angle = 0;  % rad
+smiData.RigidTransform(2).axis = [0 0 0];
+smiData.RigidTransform(2).ID = 'UCS[pendulum*:*Default:cord_pendulumTop]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [0 60.000000000000057 24.999999999999911];  % mm
+smiData.RigidTransform(3).angle = 0;  % rad
+smiData.RigidTransform(3).axis = [0 0 0];
+smiData.RigidTransform(3).ID = 'B[xeCart-2:-:pendulum-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [0 0 25];  % mm
+smiData.RigidTransform(4).angle = 0;  % rad
+smiData.RigidTransform(4).axis = [0 0 0];
+smiData.RigidTransform(4).ID = 'F[xeCart-2:-:pendulum-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [0 10.000000000000009 0];  % mm
+smiData.RigidTransform(5).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(5).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(5).ID = 'B[base-1:-:xeCart-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [-131.50649261755547 -70 0];  % mm
+smiData.RigidTransform(6).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(6).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(6).ID = 'F[base-1:-:xeCart-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [-582.59737787468589 828.09278431763823 -1960.7939464535255];  % mm
+smiData.RigidTransform(7).angle = 0;  % rad
+smiData.RigidTransform(7).axis = [0 0 0];
+smiData.RigidTransform(7).ID = 'RootGround[base-1]';
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(3).mass = 0.0;
+smiData.Solid(3).CoM = [0.0 0.0 0.0];
+smiData.Solid(3).MoI = [0.0 0.0 0.0];
+smiData.Solid(3).PoI = [0.0 0.0 0.0];
+smiData.Solid(3).color = [0.0 0.0 0.0];
+smiData.Solid(3).opacity = 0.0;
+smiData.Solid(3).ID = '';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 13.750000000000002;  % kg
+smiData.Solid(1).CoM = [0 5 0];  % mm
+smiData.Solid(1).MoI = [447705.72916666674 5993424.4791666688 5545947.9166666698];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = 'base*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 7.6109601685880772;  % kg
+smiData.Solid(2).CoM = [0 -0.57821824419924861 0];  % mm
+smiData.Solid(2).MoI = [47449.057663211555 98168.539387400437 63813.024132137492];  % kg*mm^2
+smiData.Solid(2).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(2).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = 'xeCart*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.042863053218407964;  % kg
+smiData.Solid(3).CoM = [-0.00024857185427044139 229.02427649485719 0];  % mm
+smiData.Solid(3).MoI = [1025.3033472338993 1.329598083141273 1024.5102448456521];  % kg*mm^2
+smiData.Solid(3).PoI = [0 0.0001348502719104038 -0.0019510594337314173];  % kg*mm^2
+smiData.Solid(3).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = 'pendulum*:*Default';
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the PrismaticJoint structure array by filling in null values.
+smiData.PrismaticJoint(1).Pz.Pos = 0.0;
+smiData.PrismaticJoint(1).ID = '';
+
+smiData.PrismaticJoint(1).Pz.Pos = 0;  % m
+smiData.PrismaticJoint(1).ID = '[base-1:-:xeCart-2]';
+
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(1).Rz.Pos = 0.0;
+smiData.RevoluteJoint(1).ID = '';
+
+smiData.RevoluteJoint(1).Rz.Pos = 4.9999999999999938;  % deg
+smiData.RevoluteJoint(1).ID = '[xeCart-2:-:pendulum-2]';
+
